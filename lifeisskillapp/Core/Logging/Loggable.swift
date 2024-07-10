28b262c86a0dcdfc7f5ba33a0a346617dfa351be
Karolina Droscova <drosckar@fit.cvut.cz>
2024-07-10 17:34:42 +0200

    basic implementation for generic loggable event

--- lifeisskillapp/Core/Logging/Loggable.swift
+++ lifeisskillapp/Core/Logging/Loggable.swift
@@ -19,3 +19,38 @@ public protocol Loggable: CustomStringConvertible, Codable {
     var context: LogContext { get }
 }
 
+public class LogEvent: Loggable {
+    public var message: String
+    
+    public var identifier: String
+    
+    public var dateString: String
+    
+    public var source: LogSource
+    
+    public var severity: LogSeverity
+    
+    public var context: LogContext
+    
+    public var description: String {
+        (try? JsonMapper.jsonString(from: self)) ?? "Mapping Failed"
+    }
+    
+    public init(
+        fileID: String = #fileID,
+        fun: String = #function,
+        line: Int = #line,
+        message: String = "Unknown",
+        context: LogContext = .system,
+        severity: LogSeverity = .info,
+        logger: LoggerServicing
+    ) {
+        self.source = .init(fileID: fileID, fun: fun, line: line)
+        self.context = context
+        self.message = message
+        self.identifier = UUID().uuidString
+        self.dateString = Date().ISO8601Format()
+        self.severity = severity
+        logger.log(event: self)
+    }
+}
