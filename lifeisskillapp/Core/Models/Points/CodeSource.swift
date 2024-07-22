82bd23a1fc58952bf1613278bec535318d643a50
Karolina Droscova <drosckar@fit.cvut.cz>
2024-07-22 14:13:24 +0200

    PR: code source documentation

--- lifeisskillapp/Core/Models/Points/CodeSource.swift
+++ lifeisskillapp/Core/Models/Points/CodeSource.swift
@@ -8,8 +8,15 @@
 import Foundation
 
 enum CodeSource: String, Codable {
+    /// Loaded from tag with QR code
     case qr = "QR"
+
+    /// Loaded from NFC tag
     case nfc = "NFC"
+
+    /// Loaded from location, point only visible in map, no physical LiS sign/tag
     case virtual = "VIRTUAL"
+
+    /// Loaded from tourist signs
     case text = "TEXT"
 }
