9f9ca665dbfc476c1075b3f217d9ae6c885bb596
Karolina Droscova <drosckar@fit.cvut.cz>
2024-07-24 14:47:11 +0200

    corrected userdatastorage

--- lifeisskillapp/Core/Storage/UserDefaultsStorage.swift
+++ lifeisskillapp/Core/Storage/UserDefaultsStorage.swift
@@ -26,9 +26,10 @@ final class UserDefaultsStorage: UserDefaultsStoraging {
     private let logger: LoggerServicing
     
     // MARK: - Public Properties
+    
     var checkSumData: CheckSumData? {
         get {
-            return UserDefaults.standard.checkSumData
+            return UserDefaults.standard.checkSumData // uses UserDefaults extension for custom key
         }
         set {
             UserDefaults.standard.checkSumData = newValue
@@ -37,7 +38,7 @@ final class UserDefaultsStorage: UserDefaultsStoraging {
     
     var location: UserLocation? {
         get {
-            return UserDefaults.standard.location
+            return UserDefaults.standard.location // uses UserDefaults extension for custom key
         }
         set {
             UserDefaults.standard.location = newValue
@@ -46,7 +47,7 @@ final class UserDefaultsStorage: UserDefaultsStoraging {
     
     var appId: String? {
         get {
-            return UserDefaults.standard.appId
+            return UserDefaults.standard.appId // uses UserDefaults extension for custom key
         }
         set {
             UserDefaults.standard.appId = newValue
