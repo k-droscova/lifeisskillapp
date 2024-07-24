f8c1e58234afa5fcd636e84ab4b91868d6fa0266
Karolina Droscova <drosckar@fit.cvut.cz>
2024-07-24 17:20:10 +0200

    changed comments in docs

--- lifeisskillapp/LiS/Source/FlowCoordinators/HomeFlowCoordinator.swift
+++ lifeisskillapp/LiS/Source/FlowCoordinators/HomeFlowCoordinator.swift
@@ -20,14 +20,14 @@ protocol HomeFlowDelegate: NSObject {
     func loadFromPhoto()
 }
 
-/// The LoginFlowCoordinator is responsible for managing the login flow within the app. It handles the navigation and actions from the login view controller.
+/// The HomeFlowCoordinator is responsible for managing the home flow within the app. It handles the navigation and actions from the home view controller.
 final class HomeFlowCoordinator: Base.FlowCoordinatorNoDeepLink {
-    /// The delegate to notify about the success of the login process.
+    /// The delegate to notify about the success of point loading.
     weak var delegate: HomeFlowCoordinatorDelegate?
 
-    /// Starts the login flow by presenting the login view controller.
+    /// Starts the home flow by presenting the home view controller.
     ///
-    /// - Returns: The login view controller to be presented.
+    /// - Returns: The home view controller to be presented.
     override func start() -> UIViewController {
         let viewModel = HomeViewModel(dependencies: appDependencies, delegate: self)
         let homeController = HomeViewController(viewModel: viewModel)
@@ -39,13 +39,16 @@ final class HomeFlowCoordinator: Base.FlowCoordinatorNoDeepLink {
 extension HomeFlowCoordinator: HomeFlowDelegate {
     func loadWithNFC() {
         print("NFC tapped")
+        // Implement actual navigation or logic here
     }
     
     func loadWithQRCode() {
         print("QR tapped")
+        // Implement actual navigation or logic here
     }
     
     func loadFromPhoto() {
         print("Photo tapped")
+        // Implement actual navigation or logic here
     }
 }
