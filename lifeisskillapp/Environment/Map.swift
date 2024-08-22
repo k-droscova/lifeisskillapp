50d9f2d0a053b6ec30bf3861546586edd05d648b
Karolina Droscova <drosckar@fit.cvut.cz>
2024-08-22 13:46:59 +0200

    refactoring

--- lifeisskillapp/Environment/Map.swift
+++ lifeisskillapp/Environment/Map.swift
@@ -9,9 +9,9 @@ import Foundation
 import CoreLocation
 
 public struct MapConstants {
+    static let mapDetailViewSheetHeight: CGFloat = 150
     static let latitudeDelta = 0.05
     static let longitudeDelta = 0.05
-    static let maxCenterCoordinateDistance = 20000.0
     static let defaultCoordinate = CLLocation(
         coordinate: CLLocationCoordinate2D(
             latitude: Prague.latitude,
@@ -28,6 +28,4 @@ public struct MapConstants {
         static let longitude: Double = 14.418540
         static let altitude: Double = 225
     }
-    
-    static let mapDetailViewSheetHeight: CGFloat = 150
 }
