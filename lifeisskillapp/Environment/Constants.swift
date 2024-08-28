a983a3a2111d0f77ceb4930ec7b99adf0c1ffb0e
Karolina Droscova <drosckar@fit.cvut.cz>
2024-08-28 21:55:52 +0200

    pin validity for debug

--- lifeisskillapp/Environment/Constants.swift
+++ lifeisskillapp/Environment/Constants.swift
@@ -10,7 +10,7 @@ import Foundation
 enum Password {
     static let minLenght = 6
     #if DEBUG
-    static let pinValidityTime = 0.1 // in minutes
+    static let pinValidityTime = 0.5 // in minutes
     #else
     static let pinValidityTime = 15.0 // in minutes
     #endif
